2f81c30c964fe34f89eef7d37777593a338cc21d
George Fisher <george@georgefisher.com>
2016-08-10 12:35:27 -0400

    Initial commit of Puppet files

---
+++ puppet-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ `id -u` = 0 ] ; then
+
+    wget -O /tmp/puppetlabs.deb http://apt.puppetlabs.com/puppetlabs-release-$(lsb_release -cs).deb
+    if [[ ! $? -eq 0 ]]; then
+        wget -O /tmp/puppetlabs.deb http://apt.puppetlabs.com/puppetlabs-release-pc1-$(lsb_release -cs).deb
+    fi
+
+    dpkg -i /tmp/puppetlabs.deb
+    apt-get update
+    apt-get install -y puppet
+
+    if [ -f /etc/default/puppet ]; then
+        # allow the puppet agent service to run
+        sed -i 's/START=no/START=yes/' /etc/default/puppet
+    fi
+
+    # modify the conf file for masterless
+    # ===================================
+    if [ ! -f /etc/puppet/puppet.conf.bak ]; then
+        cp /etc/puppet/puppet.conf /etc/puppet/puppet.conf.bak
+    fi
+
+
+    cat > /etc/puppet/puppet.conf <<EOF
+[main]
+logdir=/var/log/puppet
+vardir=/var/lib/puppet
+ssldir=/var/lib/puppet/ssl
+rundir=/run/puppet
+factpath=/usr/lib/ruby/vendor_ruby/facter
+EOF
+
+else
+    echo "Please enter sudo su before running this script"
+fi
